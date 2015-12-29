c27fb0849e7ff3cef954b63e5ffd1ebe47084c6c
jglobant <johannes.schmidt@globant.com>
2015-12-29 19:48:16 +0000

    refactor packages

--- test/framework/marathon_test.go
+++ test/framework/marathon_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"fmt"
 	"github.com/jglobant/yale/model"
+	"github.com/jglobant/yale/framework"
 	"net/http"
 	"net/http/httptest"
 )
@@ -33,9 +34,11 @@ func TestListServices(t *testing.T) {
 }
 
 func TestDeployService(t *testing.T) {
+	content, _ := ioutil.ReadFile("test/resources/marathon_tasks_response.json")
+
 ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
                 w.Header().Set("Content-Type", "application/json")
-                fmt.Fprintln(w, `{"id":"/nginx","cmd":null,"args":null,"user":null,"env":{},"instances":1,"cpus":0.25,"mem":256.0,"disk":0.0,"executor":"","constraints":[],"uris":[],"storeUrls":[],"ports":[80],"requirePorts":false,"backoffSeconds":1,"backoffFactor":1.15,"maxLaunchDelaySeconds":3600,"container":{"type":"DOCKER","volumes":[],"docker":{"image":"nginx","network":"BRIDGE","portMappings":[{"containerPort":80,"hostPort":0,"servicePort":80,"protocol":"tcp"}],"privileged":false,"parameters":[],"forcePullImage":false}},"healthChecks":[],"dependencies":[],"upgradeStrategy":{"minimumHealthCapacity":1.0,"maximumOverCapacity":1.0},"labels":{},"acceptedResourceRoles":null,"version":"2015-12-28T18:44:32.774Z","tasksStaged":0,"tasksRunning":0,"tasksHealthy":0,"tasksUnhealthy":0,"deployments":[{"id":"b37bbd63-6e92-43b6-9199-d1ca27642b55"}],"tasks":[]}`)
+                fmt.Fprintln(w, content)
         }))
         defer ts.Close()
 	helper, error := NewMarathonHelper(ts.URL)
