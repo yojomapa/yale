8f294141766c8ee47a4b8f3836573f68497b1bb4
jglobant <johannes.schmidt@globant.com>
2015-12-30 18:00:25 +0000

    move test, adjust paths

--- test/framework/marathon_test.go
+++ test/framework/marathon_test.go
@@ -1,11 +1,11 @@
-package framework
+package test
 
 import (
 	"testing"
 	"fmt"
 	"io/ioutil"
 	"github.com/jglobant/yale/model"
-	//"github.com/jglobant/yale/framework"
+	"github.com/jglobant/yale/framework"
 	"net/http"
 	"net/http/httptest"
 )
@@ -13,14 +13,14 @@ import (
 func TestListServices(t *testing.T) {
 	
 	fmt.Println("TestListServices Starting")
-	content, _ := ioutil.ReadFile("../test/resources/marathon_tasks_response.json")
+	content, _ := ioutil.ReadFile("../resources/marathon_tasks_response.json")
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprintln(w, string(content))
 	}))
 	defer ts.Close()
 
-	helper, error := NewMarathonHelper(ts.URL)
+	helper, error := framework.NewMarathonHelper(ts.URL)
 	
 	fmt.Println("NewMarathonHelper CALLED")
 	
@@ -36,14 +36,14 @@ func TestListServices(t *testing.T) {
 
 func TestDeployService(t *testing.T) {
 fmt.Println("yeah - 0")
-	content, _ := ioutil.ReadFile("../test/resources/marathon_new_app_response.json")
+	content, _ := ioutil.ReadFile("../resources/marathon_new_app_response.json")
 fmt.Println("yeah")
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
                 w.Header().Set("Content-Type", "application/json")
                 fmt.Fprintln(w, string(content))
         }))
         defer ts.Close()
-	helper, error := NewMarathonHelper(ts.URL)
+	helper, error := framework.NewMarathonHelper(ts.URL)
 
 	fmt.Println("Marathon created")
 	if error != nil {
